e933937766a858a01309e482156ede8d0ea5abbe
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-03-25 12:13:21 +0200

    Style login page & config spotify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Login from "./Components/Login"
 import "./App.css"
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello,Spotify demo app</h1>
+      <Login />
     </div>
   )
 }
