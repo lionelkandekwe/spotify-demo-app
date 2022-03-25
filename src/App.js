419cd6d07eb1fc849663e6e19356bdbebe0153b0
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-03-25 15:02:08 +0200

    Create player component & getToken

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React, { useEffect, useState } from "react"
 import Login from "./Components/Login"
+import Player from "./Components/Player"
+import SpotifyWebApi from "spotify-web-api-js"
+import { getTokenFromUrl } from "./Config/spotify"
 import "./App.css"
 
+const spotify = new SpotifyWebApi()
+
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  )
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    const hash = getTokenFromUrl()
+
+    window.location.hash = ""
+
+    const _token = hash.access_token
+
+    if (_token) {
+      setToken(_token)
+      spotify.setAccessToken(_token)
+      spotify.getMe().then((user) => {
+        console.log("The person", user)
+      })
+    }
+
+    console.log("I HAVE A TOKEN====> ", _token)
+  }, [])
+  return <div className="app">{token ? <Player /> : <Login />}</div>
 }
 
 export default App
---
+++ src/Components/Player.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "../Assets/Styles/Player.css"
+const Player = () => {
+  return (
+    <div>
+      <h1>Welcome to Spotify</h1>
+    </div>
+  )
+}
+
+export default Player
