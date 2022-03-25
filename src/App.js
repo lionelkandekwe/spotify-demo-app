9f26b78ce3f1af09d166718d7fe57700d9bb14b1
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-03-25 17:02:21 +0200

    DataLayer config

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Login from "./Components/Login"
 import Player from "./Components/Player"
 import SpotifyWebApi from "spotify-web-api-js"
 import { getTokenFromUrl } from "./Config/spotify"
+import { useDataLayerValue } from "./Context/DataLayer"
 import "./App.css"
 
 const spotify = new SpotifyWebApi()
 
 function App() {
-  const [token, setToken] = useState(null)
+  const [{ user, token }, dispatch] = useDataLayerValue()
 
   useEffect(() => {
     const hash = getTokenFromUrl()
@@ -18,15 +19,28 @@ function App() {
     const _token = hash.access_token
 
     if (_token) {
-      setToken(_token)
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
+
       spotify.setAccessToken(_token)
+
       spotify.getMe().then((user) => {
-        console.log("The person", user)
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        })
       })
     }
 
     console.log("I HAVE A TOKEN====> ", _token)
   }, [])
+
+  console.log("The person ==> ", user)
+
+  console.log("Token ===>", token)
+
   return <div className="app">{token ? <Player /> : <Login />}</div>
 }
 
