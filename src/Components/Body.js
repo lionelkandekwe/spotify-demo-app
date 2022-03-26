d4e603ae5560267c817ff80fd93b24205c017ff1
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-03-26 16:53:28 +0200

    Style banner

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -6,6 +6,14 @@ const Body = ({ spotify }) => {
   return (
     <div className="body">
       <Header spotify={spotify} />
+      <div className="body__info">
+        <img src="" alt="" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>Description...</p>
+        </div>
+      </div>
     </div>
   )
 }
