c92eab2436a9db6c4eb400f8a9a0fdfb7c4b8801
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-03-26 17:10:25 +0200

    discover weekly

--- src/App.js
+++ src/App.js
@@ -38,6 +38,12 @@ function App() {
           playlists: playlists,
         })
       })
+      spotify.getPlaylist("37i9dQZEVXcRgAVZvVdbx4").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      )
     }
   }, [])
 
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,17 +1,19 @@
 import React from "react"
 import Header from "./Header"
+import { useDataLayerValue } from "../Context/DataLayer"
 import "../Assets/Styles/Body.css"
 
 const Body = ({ spotify }) => {
+  const [{ discover_weekly }] = useDataLayerValue()
   return (
     <div className="body">
       <Header spotify={spotify} />
       <div className="body__info">
-        <img src="" alt="" />
+        <img src={discover_weekly?.images[0].url} alt="" />
         <div className="body__infoText">
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
-          <p>Description...</p>
+          <p>{discover_weekly?.description}</p>
         </div>
       </div>
     </div>
