d2bde16d2058783437d1c85bd5d15ba855918c63
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-03-26 17:30:38 +0200

    style body icons

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,6 +1,9 @@
 import React from "react"
 import Header from "./Header"
 import { useDataLayerValue } from "../Context/DataLayer"
+import PlayCircleFilledIcon from "@mui/icons-material/PlayCircleFilled"
+import FavoriteIcon from "@mui/icons-material/Favorite"
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz"
 import "../Assets/Styles/Body.css"
 
 const Body = ({ spotify }) => {
@@ -14,6 +17,13 @@ const Body = ({ spotify }) => {
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
           <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle" />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
         </div>
       </div>
     </div>
